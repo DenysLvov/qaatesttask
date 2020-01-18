29a01dd4e3437206351468f3144bcc9f4afaa0d5
Den Lvov <lionpdv@gmail.com>
2020-01-18 15:49:51 +0200

    Add second test scenario Add StepsDefenition Fix proj. settings

--- src/test/java/testtask/TestRunner.java
+++ src/test/java/testtask/TestRunner.java
@@ -1,8 +1,30 @@
 package testtask;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberFeatureWrapper;
 import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.TestNGCucumberRunner;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-@CucumberOptions (features = {"src/test/resources"}, glue="src.test.java.steps")
-public class TestRunner extends AbstractTestNGCucumberTests{
+@CucumberOptions (features = {"src/test/resources/"}, glue="steps")
+public class TestRunner {
+    private TestNGCucumberRunner testNGCucumberRunner;
 
+    @BeforeClass(alwaysRun = true)
+    public void setUpClass() throws Exception {
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+    }
+
+    @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
+    public void feature(CucumberFeatureWrapper cucumberFeature) {
+        testNGCucumberRunner.runScenario(cucumberFeature.getClass().);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDownClass() throws Exception {
+        testNGCucumberRunner.finish();
+    }
 }
